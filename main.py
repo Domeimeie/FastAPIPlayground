69cbb11a513568275fcf85fc28ae7866562a8b3b
Dominique <schlurpf@gmail.com>
2026-02-10 19:22:38 +0100

    Implemented simple fake login validation API

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from fastapiplayground!")
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/status")
+async def getStatus():
+    return {"status": "ok"}
+
+@app.post("/login")
+async def login(loginData: LoginData):
+    if loginData.username == "test" and loginData.password == "1234":
+        return {"login": "ok"}
+    return JSONResponse(status_code=401, content={"status": "error"})
